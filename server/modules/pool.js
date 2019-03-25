03d79d6e0323dc9e3be69bf3137a89f324b0aa65
Ben Ragsdale <ben.ragsdale@gmail.com>
2019-03-25 12:04:06 -0500

    pool.js comment out const.pool ln 3

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,12 @@
 const pg = require("pg");
 const Pool = pg.Pool;
-const pool = new Pool({
-  database: "weekend-to-do-app",
-  host: "localhost",
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000
-});
+// const pool = new Pool({
+//   database: "weekend-to-do-app",
+//   host: "localhost",
+//   port: 5432,
+//   max: 10,
+//   idleTimeoutMillis: 30000
+// });
 const url = require("url");
 let config = {};
 
